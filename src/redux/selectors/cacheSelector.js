1671456ee7bcc971818cf1603c39eca29c2e96e4
iremlaya <iremkaya1999@hotmail.com>
2020-04-08 14:45:20 +0300

    Cache :: Combine key selectors

--- src/redux/selectors/cacheSelector.js
+++ src/redux/selectors/cacheSelector.js
@@ -1,15 +1,40 @@
-import createCachedSelector from 're-reselect';
+import createCachedSelector, { LruMapCache } from 're-reselect';
 import service from '../../services/api/unsplash';
+
+function keySelectorCombiner({ inputSelectors = [] } = {}) {
+  const keySelectors = inputSelectors
+    .map((entry) => entry.keySelector)
+    .filter((keySelector) => typeof keySelector === 'function');
+
+  // The actual keySelector
+  return (...args) => keySelectors
+    .map((keySelector) => keySelector(...args))
+    .filter((value) => {
+      const valueType = typeof value;
+      return valueType === 'string' || valueType === 'number';
+    })
+    .join(':');
+}
+
+const keySelector1 = (input) => input;
+const keySelector2 = (collectionIds) => collectionIds;
+
+const keySelector = keySelectorCombiner(keySelector1, keySelector2);
 
 const cachedSelector = createCachedSelector(
   (input) => input,
+  (collectionIds) => collectionIds,
+  (page) => page,
   // Return and retain a Promise as a result
   (input, collectionIds, page) => service.searchPhotosByCollections(input, collectionIds, page),
-)(
+)({
   /*
      * Re-reselect resolver function.
      * Cache a new selector for each new parameter
      */
-  (input) => input,
-);
+  keySelector,
+  cacheObject: new LruMapCache({ cacheSize: 5 }),
+  // or:
+  // cacheObject: new LruMapCache({cacheSize: 5}),
+});
 export default cachedSelector;
